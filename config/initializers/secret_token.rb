e55983855af681fa9b806ce0beb48200cb650543
Nisha Sehn <nishasehn@gmail.com>
2013-02-10 16:24:35 -0800

    Initializes a blank Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraigslistScraper::Application.config.secret_token = '856c65d3cb3cb621bdfcffd883c8f7c0a005345f48931e7a681157f0765100e325190d382a864cf8f79541eb89c94283ac8055c522a18360139ac2d663e6a371'
