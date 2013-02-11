bafba1016b8b01af47b5c2e3c9f2857cb696fc54
Nisha Sehn <nishasehn@gmail.com>
2013-02-10 20:42:01 -0800

    Refactors to create a Craigslist Scraper class which allows any url to be scraped.

--- lib/craigslist_scraper.rb
+++ lib/craigslist_scraper.rb
@@ -1,14 +1,24 @@
 require 'nokogiri' #scraping/parsing library
 require 'open-uri' #part of library that allows user to download webpage
 
-url = "http://sfbay.craigslist.org/bia/"
-data = Nokogiri::HTML(open(url))
-rows = data.css('.row')
+class CraigslistScraper
 
-rows.each do |row|
-	category = row.at_css('.itemcg').text
-	title = row.at_css('a').text
-	price = row.at_css('.itempp').text.delete(" $").to_i
-	location = row.at_css('.itempn').text
-	link = row.at_css('a')['href']
+	def initialize(url)
+		@data = Nokogiri::HTML(open(url))
+	end
+
+	def get_data
+		rows = data.css('.row')
+
+		rows.each do |row|
+			category = row.at_css('.itemcg').text
+			title = row.at_css('a').text
+			price = row.at_css('.itempp').text.delete(" $").to_i
+			location = row.at_css('.itempn').text
+			link = row.at_css('a')['href']
+			item = CraigslistItem.new(:category => category, :title => title, :price => price, :location => location, :url => link)
+			item.save
+		end
+	end
+
 end
