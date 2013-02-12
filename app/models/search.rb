b7a0f361006e27c98571f9f557f8dde59c9d9964
Nisha Sehn <nishasehn@gmail.com>
2013-02-11 22:36:19 -0800

    Creates link to the Craigslist page after entering search parameters. Next step is to scrape the url instead of directing user to it.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,8 +1,8 @@
 class Search < ActiveRecord::Base
   attr_accessible :category, :location, :max_price
 
-  def get_url
-  return "http://sfbay.craigslist.org/search/" + :category + "/" + :location + "?zoomToPosting=&altView=&query=&srchType=A&minAsk=&maxAsk=" + :max_price
+  def get_url()
+  return "http://sfbay.craigslist.org/search/" + self.category + "/" + self.location + "?zoomToPosting=&altView=&query=&srchType=A&minAsk=&maxAsk=" + self.max_price.to_s
 
   end
 end
