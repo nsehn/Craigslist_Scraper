4e715495eafc39a28c766478a0961a947b1d4792
Nisha Sehn <nishasehn@gmail.com>
2013-03-07 12:38:40 -0800

    Bookmarklet javascript beginnings. Cleaned up code, still need to fix some routing issues.

--- app/helpers/bookmarklets_helper.rb
+++ app/helpers/bookmarklets_helper.rb
@@ -1,6 +1,3 @@
 module BookmarkletsHelper
-	def bookmarklet_link_url
-		# "javascript:d=document;s=d.createElement('script');s.src='http://localhost:3000/bookmarklets?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(d.title);d.body.appendChild(s);".html_safe
-		"javascript:d=document;s=d.createElement('script');s.src='http://localhost:3000/bookmarklets/js';d.body.appendChild(s);".html_safe
-	end
+	
 end
--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,6 @@
 module SearchesHelper
+def bookmarklet_link_url
+		# "javascript:d=document;s=d.createElement('script');s.src='http://localhost:3000/bookmarklets?url='+encodeURIComponent(location.href)+'&title='+encodeURIComponent(d.title);d.body.appendChild(s);".html_safe
+		"javascript:d=document;s=d.createElement('script');s.src='http://localhost:3000/bookmarklets/js';d.body.appendChild(s);".html_safe
+	end
 end
